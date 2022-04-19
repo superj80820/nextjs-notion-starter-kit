21757dc6a04707584a26e96abc0fb55cc3949148
York lin <superj80820@gmail.com>
2022-04-20 02:24:26 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '036948842f494993a50d4166bff5346d',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '還在亂的髒桶子',
+  domain: 'note.messfar.com',
+  author: 'York Lin',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
