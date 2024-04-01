fdb366e9f5b9db77d5724af494c1906792e54c3c
York lin <superj80820@gmail.com>
2024-04-01 21:49:22 +0800

    update

--- site.config.ts
+++ site.config.ts
@@ -27,7 +27,7 @@ export default siteConfig({
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
   defaultPageIcon: "https://note.messfar.com/page-icon.png",
-  defaultPageCover: null,
+  defaultPageCover: "https://note.messfar.com/page-cover.jpg",
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
